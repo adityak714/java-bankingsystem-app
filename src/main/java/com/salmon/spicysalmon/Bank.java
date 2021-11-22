c8a64a4c67d1e9d2da0c6bd5d17d19240c7bf4c8
viccam <viccam@student.chalmers.se>
2021-11-22 16:50:35 +0100

    Added createCustomer method

--- src/main/java/com/salmon/spicysalmon/Bank.java
+++ src/main/java/com/salmon/spicysalmon/Bank.java
@@ -2,15 +2,27 @@ package com.salmon.spicysalmon;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 public class Bank {
 
-    private HashMap<User> userCollection;
-    private HashMap<ArrayList<Transaction>> transactions;
+    private HashMap<int, User> usersCollection;
+    private LinkedHashMap<Integer, ArrayList<Transaction>> transactions;
 
     public Bank() {
         this.userCollection = new HashMap();
         this.transactions = new HashMap();
     }
 
+    public void createCustomer(double salary,
+                                  int residentialArea, String occupation, int socialSecurityNumber){
+
+        User newCustomer = new Customer(salary, residentialArea, occupation, socialSecurityNumber);
+        int uniqueID;
+        do{
+            uniqueID = (int) (Math.random()*999999);
+        } while(transactions.get(uniqueID) != null);
+        uniqueID = Integer.parseInt("3"+ uniqueID);
+        usersCollection.put(uniqueID, newCustomer);
+    }
 }
