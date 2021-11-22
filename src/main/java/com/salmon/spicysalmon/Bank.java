0ab0fb562a0fef078c68d98b9f5fdcb06d85cba1
shariqs <shariqs@student.chalmers.se>
2021-11-22 17:04:36 +0100

    added transferBetweenCustomers method as well as minor exception handling

--- src/main/java/com/salmon/spicysalmon/Bank.java
+++ src/main/java/com/salmon/spicysalmon/Bank.java
@@ -14,15 +14,30 @@ public class Bank {
         this.transactions = new HashMap();
     }
 
-    public void createCustomer(double salary,
-                                  int residentialArea, String occupation, int socialSecurityNumber){
+    public void createCustomer(double salary, int residentialArea, String occupation, int socialSecurityNumber) throws Exception{
+        try{
+            User newCustomer = new Customer(salary, residentialArea, occupation, socialSecurityNumber);
+            int uniqueID;
+            do{
+                uniqueID = (int) (Math.random()*999999);
+            } while(transactions.get(uniqueID) != null);
+            uniqueID = Integer.parseInt("3"+ uniqueID);
+            usersCollection.put(uniqueID, newCustomer);
+        } catch(Exception e){
+            throw new Exception("Please enter valid customer information");
+        }
+    }
 
-        User newCustomer = new Customer(salary, residentialArea, occupation, socialSecurityNumber);
-        int uniqueID;
-        do{
-            uniqueID = (int) (Math.random()*999999);
-        } while(transactions.get(uniqueID) != null);
-        uniqueID = Integer.parseInt("3"+ uniqueID);
-        usersCollection.put(uniqueID, newCustomer);
+    // unsure about type for from account
+    public void transferBetweenCustomers(Account from, String to, double amount) throws Exception{
+        try{
+            int userID = Integer.parseInt(to.substring(1,7));
+            int accountID = Integer.parseInt(to.substring(8,9))
+            User to = usersCollection.get(userID);
+            to.deposit(accountID, amount);
+            from.withdraw(amount);
+        } catch(Exception e){
+            throw new Exception("Please enter valid transaction data.");
+        }
     }
 }
