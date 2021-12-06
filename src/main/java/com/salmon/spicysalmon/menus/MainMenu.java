3e73420a648ca7f3044ee0c0f179e8fa01b372c9
shariqs <shariqs@student.chalmers.se>
2021-12-06 16:22:04 +0100

    added menu class as well as created main menu file

---
+++ src/main/java/com/salmon/spicysalmon/menus/MainMenu.java
@@ -0,0 +1,38 @@
+package com.salmon.spicysalmon.menus;
+
+import com.salmon.spicysalmon.models.Menu;
+
+public class MainMenu {
+
+    private final String MAIN_HEADING = "Main Menu: Please choose an option below.";
+    private final String[] MAIN_OPTIONS = {
+            "Close System",
+            "Login",
+            "Apply for account",
+            "Authorized Personnel"
+    };
+
+    public void show(){
+        int userInput = 0;
+        Menu mainMenu = new Menu(MAIN_HEADING, MAIN_OPTIONS);
+        do{
+            System.out.print(mainMenu);
+            userInput = mainMenu.getValidOption();
+            switch(userInput){
+                case 1:
+                    AuthenticationController.customerLogin();
+                    break;
+                case 2:
+                    System.out.println("Application handling");
+                    break;
+                case 3:
+                    AuthenticationController.employeeLogin();
+                    break;
+                case 0:
+                default:
+                    break;
+            }
+        } while(userInput != 0);
+    }
+
+}
