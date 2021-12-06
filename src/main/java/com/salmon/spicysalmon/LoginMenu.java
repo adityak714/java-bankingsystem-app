8d189285b78b3ea1cfa6980dabba009dd21be3bf
shariqs <shariqs@student.chalmers.se>
2021-12-06 16:53:50 +0100

    added customer menu upon successful user verification

--- src/main/java/com/salmon/spicysalmon/LoginMenu.java
+++ src/main/java/com/salmon/spicysalmon/LoginMenu.java
@@ -3,13 +3,14 @@ package com.salmon.spicysalmon;
 import com.salmon.spicysalmon.controllers.CustomerController;
 import com.salmon.spicysalmon.controllers.EmployeeController;
 import com.salmon.spicysalmon.models.Customer;
+import com.salmon.spicysalmon.models.Menu;
 
 public class LoginMenu {
     String CUSTOMER_HEADING = "Customer Menu: Please choose a valid option.";
     String[] CUSTOMER_OPTIONS = {
+            "Log out",
             "View Bank Accounts",
-            "Apply for new Bank Account",
-            "Log out"
+            "Apply for new Bank Account"
     };
 
 
@@ -27,10 +28,24 @@ public class LoginMenu {
         CustomerController customerController = new CustomerController();
         Customer loggedInCustomer = customerController.getCustomer(SSN);
         if(loggedInCustomer != null && loggedInCustomer.verifyPassword(password)){
-            Menu customerMenu = new Menu(CUSTOMER_HEADING, CUSTOMER_OPTIONS);
+            showCustomerMenu();
         } else{
             System.out.println("Username or password incorrect.");
         }
+    }
+
+    private void showCustomerMenu(){
+        Menu customerMenu = new Menu(CUSTOMER_HEADING, CUSTOMER_OPTIONS);
+        int userInput = 0;
+        do{
+            System.out.print(customerMenu);
+            userInput = customerMenu.getValidOption();
+            switch (userInput) {
+                case 1 -> System.out.println("View Bank Accounts");
+                case 2 -> System.out.println("apply new bank account");
+                case 0, default -> {}
+            }
+        }while(userInput != 0);
     }
 
     public void loginEmployee(){
