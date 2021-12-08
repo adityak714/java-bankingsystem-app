8309a98d822f43bd455cad4187f585e4d45afd6d
adityaa <adityaa@student.chalmers.se>
2021-12-08 13:26:19 +0100

    wrote method for sorting the transactions in ONE account by price (Most Expensive to Least Expensive) and printing the sorted list

--- src/main/java/com/salmon/spicysalmon/controllers/TransactionController.java
+++ src/main/java/com/salmon/spicysalmon/controllers/TransactionController.java
@@ -1,12 +1,16 @@
 package com.salmon.spicysalmon.controllers;
 
+import com.salmon.spicysalmon.models.Customer;
 import com.salmon.spicysalmon.models.Transaction;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.LinkedHashMap;
+import java.util.List;
 
 public class TransactionController {
 
     private final LinkedHashMap<String, ArrayList<Transaction>> allTransactions;
+    CustomerController customerController = new CustomerController();
 
     public TransactionController(){
         allTransactions = new LinkedHashMap<>();
@@ -14,5 +18,16 @@ public class TransactionController {
 
     public boolean checkIfSSNUnique(String SSN){ // Armin: use verb when naming methods
         return allTransactions.get(SSN) == null;
+    }
+
+    public String sortByPriceOneAccount (String SSN, String accID){
+        Customer desiredCustomer = customerController.findCustomer(SSN);
+        String sortedTransactions = "";
+        ArrayList <Transaction> sortedList = allTransactions.get(SSN).get(accID).sort(Comparator.comparingDouble(Transaction::getAMOUNT));
+        for(Transaction transaction : sortedList){
+            sortedTransactions += transaction;
+        }
+
+        return sortedTransactions;
     }
 }
