485961fde7d740ee08b81b92a37cd0c211712320
umarma <umarma@student.chalmers.se>
2021-12-08 13:21:48 +0100

    made methods for displaying Transactions for accounts and customers

--- src/main/java/com/salmon/spicysalmon/controllers/TransactionController.java
+++ src/main/java/com/salmon/spicysalmon/controllers/TransactionController.java
@@ -1,5 +1,7 @@
 package com.salmon.spicysalmon.controllers;
 
+import com.salmon.spicysalmon.models.BankAccountApplication;
+import com.salmon.spicysalmon.models.Customer;
 import com.salmon.spicysalmon.models.Transaction;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
@@ -7,6 +9,9 @@ import java.util.LinkedHashMap;
 public class TransactionController {
 
     private final LinkedHashMap<String, ArrayList<Transaction>> allTransactions;
+    private final String EOL = System.lineSeparator();
+
+    CustomerController customerController = new CustomerController();
 
     public TransactionController(){
         allTransactions = new LinkedHashMap<>();
@@ -14,5 +19,27 @@ public class TransactionController {
 
     public boolean checkIfSSNUnique(String SSN){ // Armin: use verb when naming methods
         return allTransactions.get(SSN) == null;
+    }
+    public String printTransactionsForAnAccount(String SSN, String accID){
+        String transactionForAnAccount="";
+        Customer customer = customerController.findCustomer(SSN);
+        if(customer!=null){
+            for(Transaction transaction : allTransactions.get(SSN).get(accID){
+                transactionForAnAccount += transaction;
+            }
+        }
+        return transactionForAnAccount;
+    }
+    public String printTransactionsForAllAccounts(String SSN){
+        String transactionsForAllAccounts = "";
+        Customer customer = customerController.findCustomer(SSN);
+        if(customer!=null){
+            for(String accountKey : allTransactions.get(SSN).keySet()){
+                for(Transaction transaction : allTransactions.get(SSN).get(accountKey)){
+                    transactionsForAllAccounts += transaction + EOL;
+                }
+            }
+        }
+        return transactionsForAllAccounts;
     }
 }
