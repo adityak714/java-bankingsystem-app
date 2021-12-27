08c62a42a7092920f86e1535a3c05380b7649058
shariqs <shariqs@student.chalmers.se>
2021-12-27 22:51:26 +0100

    created the method to read the information and create a new customerAccountRequest as well as moved password method into Util, and added another paramter for user to confirm their password

--- src/main/java/com/salmon/spicysalmon/menus/MainMenu.java
+++ src/main/java/com/salmon/spicysalmon/menus/MainMenu.java
@@ -1,5 +1,7 @@
 package com.salmon.spicysalmon.menus;
 
+import com.salmon.spicysalmon.Util;
+import com.salmon.spicysalmon.controllers.AccountRequestController;
 import com.salmon.spicysalmon.controllers.AuthenticationController;
 import com.salmon.spicysalmon.models.Menu;
 
@@ -25,7 +27,7 @@ public class MainMenu {
                     authenticationController.customerLogin();
                     break;
                 case 2:
-                    System.out.println("Application handling");
+                    createCustomerAccountRequest();
                     break;
                 case 3:
                     authenticationController.employeeLogin();
@@ -33,5 +35,20 @@ public class MainMenu {
                     break;
             }
         } while(userInput != 0);
+    }
+
+    // read the components necessary for a customer account request and then create that request
+    public void createCustomerAccountRequest(){
+        AccountRequestController accountRequestController = new AccountRequestController();
+        String firstName = Util.readLine("Enter your first name: ");
+        String lastName = Util.readLine("Enter your last name: ");
+        String password = Util.readNewPassword();
+        String SSN = Util.readLine("Enter your social security number: ");
+        double salary = Util.readDouble("Enter your salary: ");
+        String residentialArea = Util.readLine("Enter your area of residence: ");
+        String occupation = Util.readLine("Enter your occupation: ");
+        accountRequestController.createCustomerAccountRequest(SSN, password, firstName, lastName, salary, residentialArea, occupation);
+        System.out.println("Your application has been sent. We will inform via mail when we have made a decision");
+        System.out.println("Thank you for choosing Spicy Salmon Bank!");
     }
 }
