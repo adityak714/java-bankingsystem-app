7443f617a9f58cca732408048da27a5283b27b52
Armin Balesic <gusbalar@student.gu.se>
2021-12-30 22:15:27 +0100

    refactored printAllAccounts in CustomerController.java

--- src/main/java/com/salmon/spicysalmon/controllers/CustomerController.java
+++ src/main/java/com/salmon/spicysalmon/controllers/CustomerController.java
@@ -229,7 +229,10 @@ public class CustomerController {
                 return "No bank accounts exist for you";
             } else {
                 for (BankAccount account : customer.getBankAccounts()) {
-                    message += account + Util.EOL;
+                    message += account.getAccountName() + " account" + Util.EOL +
+                            "Account ID: " + account.getAccountNumber() + Util.EOL +
+                            "Balance: " + account.getBalance() + " SEK" + Util.EOL +
+                            Util.EOL;
                 }
                 return message;
             }
